a35e80c4450905ea5ed27ae756fa82a81fec953f
RustemAshirmametov <mametich@rambler.ru>
2024-08-17 18:39:41 +0300

    task 008 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,36 +1,42 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.os.bundleOf
-
-private const val TEXT = "text"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.changeButton)
+        val button: Button = findViewById(R.id.hideButton)
         textView = findViewById(R.id.titleTextView)
 
         button.setOnClickListener {
-            textView.text = "I am an Android Developer!"
+            textView.visibility = View.INVISIBLE
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(TEXT, textView.text.toString())
+        outState.putInt(TEXT, textView.visibility)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        textView.text = savedInstanceState.getString(TEXT)
+        textView.visibility = savedInstanceState.getInt(TEXT)
+    }
+
+    companion object {
+        private const val TEXT = "text"
     }
 }
+
